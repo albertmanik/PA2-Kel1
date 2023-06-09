996154391370d6a46140828ab0d06ce7ff68d549
albertmanik <albertmanik2808@gmail.com>
2023-06-09 16:43:24 +0700

    View Admin

--- resources/views/pages/admin/bouquet/create.blade.php
+++ resources/views/pages/admin/bouquet/create.blade.php
@@ -167,39 +167,3 @@
             </div>
         </div>
 </x-admin-layout>
-
-{{-- <form action="javascript:void(0);">
-    <div class="row gy-2 gx-3 mb-3 align-items-center">
-        <div class="col-sm-auto">
-            <label class="visually-hidden" for="autoSizingInput">Name</label>
-            <input type="text" class="form-control" id="autoSizingInput" placeholder="Jane Doe">
-        </div><!--end col-->
-        <div class="col-sm-auto">
-            <label class="visually-hidden" for="autoSizingInputGroup">Username</label>
-            <div class="input-group">
-                <div class="input-group-text">@</div>
-                <input type="text" class="form-control" id="autoSizingInputGroup" placeholder="Username">
-            </div>
-        </div><!--end col-->
-        <div class="col-sm-auto">
-            <label class="visually-hidden" for="autoSizingSelect">Preference</label>
-            <select class="form-select" id="autoSizingSelect">
-                <option selected>Choose...</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-            </select>
-        </div><!--end col-->
-        <div class="col-sm-auto">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="autoSizingCheck">
-                <label class="form-check-label" for="autoSizingCheck">
-                    Remember me
-                </label>
-            </div>
-        </div><!--end col-->
-        <div class="col-sm-auto">
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </div><!--end col-->
-    </div><!--end row-->
-</form> --}}
--- resources/views/pages/admin/papanbunga/main.blade.php
+++ resources/views/pages/admin/papanbunga/main.blade.php
@@ -89,15 +89,8 @@
                                                                         class="btn btn-sm btn-success edit-item-btn">
                                                                         Edit
                                                                     </a>
-                                                                    {{-- <button class="btn btn-sm btn-success edit-item-btn"
-                                                                        data-bs-toggle="modal"
-                                                                        data-bs-target="#showModal{{ $item->id }}">Edit</button> --}}
                                                                 </div>
                                                                 <div class="remove">
-                                                                    {{-- <a href="{{ route('admin.papanbunga.destroy', $item->id) }}"
-                                                                        class="btn btn-sm btn-danger remove-item-btn">
-                                                                        Hapus
-                                                                    </a> --}}
                                                                     <form
                                                                         onsubmit="return confirm('Apakah Anda Yakin ?');"
                                                                         action="{{ route('admin.papanbunga.destroy', $item->id) }}"
@@ -107,142 +100,11 @@
                                                                         <button type="submit"
                                                                             class="btn btn-sm btn-danger remove-item-btn">Hapus</button>
                                                                     </form>
-                                                                    {{-- <button
-                                                                        class="btn btn-sm btn-danger remove-item-btn"
-                                                                        data-bs-toggle="modal"
-                                                                        data-bs-target="#deleteRecordModal{{ $item->id }}">Remove</button> --}}
                                                                 </div>
                                                             </div>
                                                         </td>
                                                     </tr>
                                                     <!-- end row -->
-                                                    {{-- <div class="modal fade" id="showModal{{ $item->id }}"
-                                                        tabindex="-1" aria-labelledby="exampleModalLabel"
-                                                        aria-hidden="true">
-                                                        <div class="modal-dialog modal-dialog-centered">
-                                                            <div class="modal-content">
-                                                                <div class="modal-header bg-light p-3">
-                                                                    <h5 class="modal-title" id="exampleModalLabel"></h5>
-                                                                    <button type="button" class="btn-close"
-                                                                        data-bs-dismiss="modal" aria-label="Close"
-                                                                        id="close-modal"></button>
-                                                                </div>
-                                                                <form action="{{ route('store.update', $item->id) }}"
-                                                                    method="POST">
-                                                                    @method('PUT')
-                                                                    @csrf
-                                                                    <!-- Modal -->
-                                                                    <div class="modal-body">
-                                                                        <div class="mb-3">
-                                                                            <label for="customername-field"
-                                                                                class="form-label">Nama Product</label>
-                                                                            <input type="text"
-                                                                                id="customername-field"
-                                                                                class="form-control"
-                                                                                placeholder="Nama Toko" name="nama_toko"
-                                                                                value="{{ $item->name }}" />
-                                                                        </div>
-                                                                        <div class="mb-3">
-                                                                            <label for="email-field"
-                                                                                class="form-label">Alamat</label>
-                                                                            <input type="text" name="alamat"
-                                                                                id="email-field" class="form-control"
-                                                                                placeholder="Alamat"
-                                                                                value="{{ $item->alamat }}" />
-                                                                        </div>
-                                                                        <div>
-                                                                            <label for="categroy"
-                                                                                class="form-label">Kategori</label>
-                                                                            <select
-                                                                                class="form-control @error('category_id') is-invalid @enderror mt-3"
-                                                                                name="category_id">
-                                                                                <option selected disabled>Pilih Kategori
-                                                                                    Produk</option>
-                                                                                @foreach ($category as $item)
-                                                                                    <option
-                                                                                        value="{{ $item->id }}"
-                                                                                        {{ $papanbunga->category_id == $item->id ? 'selected' : '' }}>
-                                                                                        {{ $item->name }}</option>
-                                                                                @endforeach
-                                                                            </select>
-                                                                        </div>
-                                                                        <div>
-                                                                            <label for="status-field"
-                                                                                class="form-label">Status</label>
-                                                                            <select class="form-control"
-                                                                                name="status" data-trigger
-                                                                                name="status-field" id="status-field">
-                                                                                <option value="">Status</option>
-                                                                                <option value="Aktif"
-                                                                                    {{ $item->status == 'Aktif' ? 'selected' : '' }}>
-                                                                                    Aktif</option>
-                                                                                <option value="Tidak Aktif"
-                                                                                    {{ $item->status == 'Tidak Aktif' ? 'selected' : '' }}>
-                                                                                    Non Aktif</option>
-                                                                            </select>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="modal-footer">
-                                                                        <div class="hstack gap-2 justify-content-end">
-                                                                            <button type="button"
-                                                                                class="btn btn-light"
-                                                                                data-bs-dismiss="modal">Close</button>
-                                                                            <button type="submit"
-                                                                                class="btn btn-success"
-                                                                                id="edit-btn">Update</button>
-                                                                        </div>
-                                                                    </div>
-                                                                    <!--end modal -->
-                                                                </form>
-                                                            </div>
-                                                        </div>
-                                                    </div> --}}
-
-                                                    {{-- <div class="modal fade zoomIn"
-                                                        id="deleteRecordModal{{ $item->id }}" tabindex="-1"
-                                                        aria-hidden="true">
-                                                        <div class="modal-dialog modal-dialog-centered">
-                                                            <div class="modal-content">
-                                                                <div class="modal-header">
-                                                                    <button type="button" class="btn-close"
-                                                                        data-bs-dismiss="modal" aria-label="Close"
-                                                                        id="btn-close"></button>
-                                                                </div>
-                                                                <form action="{{ route('store.destroy', $item->id) }}"
-                                                                    method="POST">
-                                                                    @method('delete')
-                                                                    @csrf
-                                                                    <div class="modal-body">
-                                                                        <div class="mt-2 text-center">
-                                                                            <lord-icon
-                                                                                src="https://cdn.lordicon.com/gsqxdxog.json"
-                                                                                trigger="loop"
-                                                                                colors="primary:#f7b84b,secondary:#f06548"
-                                                                                style="width:100px;height:100px">
-                                                                            </lord-icon>
-                                                                            <div class="mt-4 pt-2 fs-15 mx-4 mx-sm-5">
-                                                                                <h4>Are you Sure ?</h4>
-                                                                                <p class="text-muted mx-4 mb-0">Are you
-                                                                                    Sure You want to Remove this Record
-                                                                                    ?
-                                                                                </p>
-                                                                            </div>
-                                                                        </div>
-                                                                        <div
-                                                                            class="d-flex gap-2 justify-content-center mt-4 mb-2">
-                                                                            <button type="button"
-                                                                                class="btn w-sm btn-light"
-                                                                                data-bs-dismiss="modal">Close</button>
-                                                                            <button type="submit"
-                                                                                class="btn w-sm btn-danger "
-                                                                                id="delete-record">Yes, Delete
-                                                                                It!</button>
-                                                                        </div>
-                                                                    </div>
-                                                                </form>
-                                                            </div>
-                                                        </div>
-                                                    </div> --}}
                                                 @endforeach
                                             </tbody>
                                         </table>
@@ -278,63 +140,6 @@
                 <!-- container-fluid -->
             </div>
             <!-- End Page-content -->
-        </div>
-
-        <div class="modal fade" id="showModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header bg-light p-3">
-                        <h5 class="modal-title" id="exampleModalLabel"></h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
-                            id="close-modal"></button>
-                    </div>
-                    <form>
-                        <!-- Modal -->
-                        <div class="modal-body">
-                            <div class="mb-3" id="modal-id" style="display: none;">
-                                <label for="id-field" class="form-label">ID</label>
-                                <input type="text" id="id-field" class="form-control" placeholder="ID"
-                                    readonly />
-                            </div>
-                            <div class="mb-3">
-                                <label for="customername-field" class="form-label">Customer Name</label>
-                                <input type="text" id="customername-field" class="form-control"
-                                    placeholder="Enter Name" required />
-                            </div>
-                            <div class="mb-3">
-                                <label for="email-field" class="form-label">Email</label>
-                                <input type="email" id="email-field" class="form-control"
-                                    placeholder="Enter Email" required />
-                            </div>
-                            <div class="mb-3">
-                                <label for="phone-field" class="form-label">Phone</label>
-                                <input type="text" id="phone-field" class="form-control"
-                                    placeholder="Enter Phone no." required />
-                            </div>
-                            <div class="mb-3">
-                                <label for="date-field" class="form-label">Joining Date</label>
-                                <input type="text" id="date-field" class="form-control" placeholder="Select Date"
-                                    required />
-                            </div>
-                            <div>
-                                <label for="status-field" class="form-label">Status</label>
-                                <select class="form-control" data-trigger name="status-field" id="status-field">
-                                    <option value="">Status</option>
-                                    <option value="Active">Active</option>
-                                    <option value="Block">Block</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <div class="hstack gap-2 justify-content-end">
-                                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
-                                <button type="submit" class="btn btn-success" id="add-btn">Add Customer</button>
-                            </div>
-                        </div>
-                        <!--end modal -->
-                    </form>
-                </div>
-            </div>
         </div>
     </div>
 </x-admin-layout>
